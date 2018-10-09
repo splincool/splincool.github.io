3c838422d2c8a014ee22267a5a53742afaf6a93a
Maksim Ivanov <maksim.ivanov@altenar.com>
2018-10-09 23:29:31 +0300

    added a new project

---
+++ projects/marioplan/precache-manifest.0900e934e4bec298bdb76683a943493b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3978eb81d40e28b097aa",
+    "url": "/static/js/main.3978eb81.chunk.js"
+  },
+  {
+    "revision": "08c8644b4e066ad9a47d",
+    "url": "/static/js/1.08c8644b.chunk.js"
+  },
+  {
+    "revision": "3978eb81d40e28b097aa",
+    "url": "/static/css/main.8f350d23.chunk.css"
+  },
+  {
+    "revision": "a06f7a0b1d64927b9ae66a4f39074086",
+    "url": "/index.html"
+  }
+];
