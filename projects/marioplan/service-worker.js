5432df30b98295d1eae61af7127acd72c9edc224
Maksim Ivanov <maksim.ivanov@altenar.com>
2018-10-09 23:38:34 +0300

    added changes for the marioplan project

--- projects/marioplan/service-worker.js
+++ projects/marioplan/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0900e934e4bec298bdb76683a943493b.js"
+  "/projects/marioplan/precache-manifest.37efc01753fc1c8a5838bc43fbfb2675.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/projects/marioplan/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
